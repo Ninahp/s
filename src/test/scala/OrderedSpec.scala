5b68c3ba7c3cda5c659b5abd9b761451a062f6b5
samuel <awesomeorji@gmail.com>
2018-11-23 09:52:04 +0100

    Tests passing

--- src/test/scala/OrderedSpec.scala
+++ src/test/scala/OrderedSpec.scala
@@ -1,42 +1,43 @@
-//import Orderer.{Accepted, ProductOrder}
-//import akka.actor.{ActorSystem, Props}
-//import akka.http.scaladsl.model._
-//import akka.testkit.{ImplicitSender, TestKit}
-//import org.scalatest.{WordSpec, WordSpecLike}
-//
-//import scala.concurrent.Future
-//
-//class OrderedSpec extends TestKit(ActorSystem("switch-test-service"))
-//  with WordSpecLike
-//  with ImplicitSender
-//  with TestHttp {
-//
-//  val test_actor  =  system.actorOf(Props(new Orderer(){
-//    override def sendHttpRequest(req : HttpRequest) = Future.successful(getHttpResponse(req))
-//  }))
-//
-//  val mockRequest = ProductOrder("Ugali",5)
-//
-//  "Orderer" must {
-//
-//    "receive produtc order request" in {
-//      test_actor ! mockRequest
-//      expectMsg(Accepted)
-//    }
-//  }
-//
-//  override def getHttpResponseImpl(data: String): ATHttpClientResponse = {
-//   data match {
-//
-//    case _  =>
-//      ATHttpClientResponse(
-//          status = StatusCode.int2StatusCode(200),
-//          data   =
-//            """
-//              |{"status":"Accepted"}
-//            """.stripMargin
-//        )
-//    }
-//  }
-//
-//}
+import akka.actor.{ActorSystem, Props}
+import akka.http.scaladsl.model._
+import akka.testkit.{ImplicitSender, TestKit}
+import com.africasTalking.elmer.core.Orderer
+import com.africasTalking.elmer.core.Orderer.{Accepted, Failure, ProductOrder, ProductResponse}
+import org.scalatest.{WordSpec, WordSpecLike}
+
+import scala.concurrent.Future
+
+class OrderedSpec extends TestKit(ActorSystem("switch-test-service"))
+  with WordSpecLike
+  with ImplicitSender
+  with TestHttp {
+
+  val test_actor  =  system.actorOf(Props(new Orderer(){
+    override def sendHttpRequest(request: HttpRequest): Future[ProductResponse] = Future.successful(getHttpResponse(request))
+  }))
+
+  val mockRequest = ProductOrder("Ugali",5)
+
+  "Orderer" must {
+
+    "receive product order request" in {
+      test_actor ! mockRequest
+      expectMsg(Accepted)
+    }
+  }
+
+  override def getHttpResponseImpl(data: String): ProductResponse = {
+   data match {
+
+    case _  =>
+      ProductResponse(
+          status = StatusCode.int2StatusCode(200),
+          data   =
+            """
+              |{"status":"Accepted"}
+            """.stripMargin
+        )
+    }
+  }
+
+}
