f4023c211cdbdecf981117c5655a07163b0e8b1c
KeeganObonyo <keeganobonyo@gmail.com>
2018-11-26 17:08:08 +0300

    Test for Elmerconfig added

---
+++ core/src/test/scala/com/africasTalking/elmer/core/config/ElmerConfigSpec.scala
@@ -0,0 +1,34 @@
+package com.africasTalking.elmer.core
+package config
+
+import org.scalatest.FlatSpec
+
+import io.atlabs._
+
+import horus.core.util.ATEnum.ATEnvironment
+
+class ElmerConfigSpec extends FlatSpec {
+
+  object TestDevConfig extends ElmerConfigT {
+    override protected def getEnvironmentImpl = "dev"
+  }
+
+  object TestStagingConfig extends ElmerConfigT {
+    override protected def getEnvironmentImpl = "staging"
+  }
+
+  object TestSandboxConfig extends ElmerConfigT {
+    override protected def getEnvironmentImpl = "sandbox"
+  }
+
+  object TestProdConfig extends ElmerConfigT {
+    override protected def getEnvironmentImpl = "prod"
+  }
+
+  "The ATConfig" should "come up correctly" in {
+    assert(TestDevConfig.getEnvironment == ATEnvironment.Development)
+    assert(TestStagingConfig.getEnvironment == ATEnvironment.Staging)
+    assert(TestSandboxConfig.getEnvironment == ATEnvironment.Sandbox)
+    assert(TestProdConfig.getEnvironment == ATEnvironment.Production)
+  }
+}
